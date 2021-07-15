298e9fbfacd04460f5b9b6c297a6679ba7066b98
Jakub Salanyk <jakub.salanyk@gmail.com>
2021-07-15 14:52:55 +0200

    initital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { DndContext, closestCenter } from '@dnd-kit/core';
+import { arrayMove, SortableContext, verticalListSortingStrategy, useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import { restrictToVerticalAxis, restrictToParentElement } from '@dnd-kit/modifiers';
+import classNames from 'classnames';
 import './App.css';
+import { coffees as data } from './data.js';
+import DragHandle from './drag-handle.svg';
 
 function App() {
+  const [items, setItems] = useState(data);
+  const [isSortEnabled, setSortEnabled] = useState(true);
+
+  const handleDragEnd = ({ active, over }) => {
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.findIndex(o => o.id === active.id);
+        const newIndex = items.findIndex(o => o.id === over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="container">
+        <DndContext
+          collisionDetection={closestCenter}
+          onDragEnd={handleDragEnd}
+          modifiers={[restrictToVerticalAxis, restrictToParentElement]}
         >
-          Learn React
-        </a>
-      </header>
+          <SortableContext
+            items={items}
+            strategy={verticalListSortingStrategy}
+          >
+            {items.map(o => <SortableItem key={o.id} data={o} disabled={!isSortEnabled} />)}
+          </SortableContext>
+        </DndContext>
+      </div>
+      <button type="button" onClick={() => setSortEnabled(prev => !prev)}>
+        {isSortEnabled ? 'disable sort' : 'enable sort'}
+      </button>
+    </>
+  );
+}
+
+const SortableItem = ({ data, disabled }) => {
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: data.id, disabled: disabled });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners} title={!disabled ? "Drag to sort" : null}
+      className={classNames({ item: true, sortable: !disabled })}>
+      <div>
+        <div className="title">{data.blend_name}</div>
+        <div className="desc">{data.origin}</div>
+      </div>
+      {!disabled && <img src={DragHandle} alt="Drag handle" className="dragHandle" />}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,49 @@
+const coffees = [
+    {
+        "id": 6876,
+        "uid": "0c02c10c-148b-42e3-ba25-0369680297a8",
+        "blend_name": "Winter Nuts",
+        "origin": "Hidalgo, Mexico",
+        "variety": "Blue Mountain",
+        "notes": "unbalanced, round, musty, lemon verbena, green-tea",
+        "intensifier": "delicate"
+    },
+    {
+        "id": 8111,
+        "uid": "1ec1b88c-7d55-47a9-8d32-7a7a7a76bca5",
+        "blend_name": "Blacktop Volcano",
+        "origin": "Chichontepec, El Salvador",
+        "variety": "Dilla",
+        "notes": "deep, chewy, pineapple, carmel, orange creamsicle",
+        "intensifier": "deep"
+    },
+    {
+        "id": 5917,
+        "uid": "91299191-f63f-4384-91c6-edc8eaa52719",
+        "blend_name": "Joe Level",
+        "origin": "Gishamwana Coffee Island, Rwanda",
+        "variety": "Typica",
+        "notes": "dry, full, black pepper, green grape, tamarind",
+        "intensifier": "complex"
+    },
+    {
+        "id": 5517,
+        "uid": "f012807c-6a7b-49f1-9a56-a2dfea5e112f",
+        "blend_name": "Cascara Equinox",
+        "origin": "Jalapa, Nicaragua",
+        "variety": "Typica",
+        "notes": "juicy, smooth, graham cracker, potato defect!, hazelnut",
+        "intensifier": "astringent"
+    },
+    {
+        "id": 5058,
+        "uid": "974f494e-4106-4aef-ada8-2a6166ff0924",
+        "blend_name": "Kreb-Full-o Forrester",
+        "origin": "Southern Region, Morogoro, Tanzania",
+        "variety": "Tafari-Kela",
+        "notes": "dirty, juicy, black-tea, snow pea, cola",
+        "intensifier": "pointed"
+    }
+];
+
+export { coffees }
